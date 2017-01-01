e5976b8f7a647af9cf5291005bc4c924ee6c6a7c
hjkcai <huajingkun1214@gmail.com>
2017-01-01 12:59:14 +0800

    deps: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  extends: 'standard',
+  rules: {
+    'arrow-parens': 0,
+    'generator-star-spacing': 0,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
+}
