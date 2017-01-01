1c40c340f3fd1a10b13b3b4298597357fc313725
hjkcai <huajingkun1214@gmail.com>
2017-01-01 13:40:32 +0800

    chore: add basic webpack config (dev + prod)

---
+++ build/webpack.config.js
@@ -0,0 +1,62 @@
+'use strict'
+
+const path = require('path')
+const merge = require('webpack-merge')
+const webpack = require('webpack')
+
+let config = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          'babel-loader',
+          'eslint-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    modules: ['node_modules']
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    })
+  ]
+}
+
+if (process.env.NODE_ENV === 'production') {
+  config = merge({
+    entry: path.resolve(__dirname, '../src/index.js'),
+    output: {
+      path: path.resolve(__dirname, '../dist'),
+      filename: 'vue-modal.js',
+      library: 'vue-modal',
+      libraryTarget: 'umd'
+    },
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      })
+    ]
+  }, config)
+} else {
+  config = merge({
+    entry: path.resolve(__dirname, '../example/app.js'),
+    output: {
+      filename: 'app.js',
+      publicPath: '/'
+    },
+    resolve: {
+      alias: {
+        'vue-modal': path.resolve(__dirname, '../src')
+      }
+    },
+    devtool: 'eval-source-map'
+  }, config)
+}
+
+module.exports = config
