e630615df16ec8491a2c68f9e73bc4a14c7ce908
hjkcai <huajingkun1214@gmail.com>
2017-01-01 23:28:00 +0800

    feat: add 'use' function and 'show' function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 'use strict'
 
+import { find } from './util'
+
 let Vue = null
 let modalFunctions = []
+
+function findModalByName (name) {
+  return find(modalFunctions, item => item.name === name)
+}
 
 export var debug = process.env.NODE_ENV === 'development'
 
@@ -9,4 +15,31 @@ export function install (vue) {
   // export vue instance to global scope
   // so that we can easily modify its prototype
   Vue = vue
+}
+
+/**
+ * Add a modal function into Vue.prototype
+ * so that you can access this function
+ * via `this.$<name>` from a Vue component.
+ */
+export function use (name, options) {
+  name = name.toString().trim()
+
+  // make sure 'name' is unique
+  if (findModalByName(name) && debug) {
+    console.warn(`[vue-modal] Another modal function ${name} is already exist.`)
+  }
+
+  modalFunctions.push(options)
+  Vue.prototype[`$${name}`] = show.bind(undefined, name)
+}
+
+export function show (name) {
+  return new Promise((resolve, reject) => {
+    if (!findModalByName(name) && debug) {
+      console.warn(`[vue-modal] Modal dialog ${name} is not found.`)
+    }
+
+    // TODO
+  })
 }
