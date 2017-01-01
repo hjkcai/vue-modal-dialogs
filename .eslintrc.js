a2457252c034655465ef395e621d1289fac1a67a
hjkcai <huajingkun1214@gmail.com>
2017-01-01 22:36:52 +0800

    chore: allow non-spaced comment

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   rules: {
     'arrow-parens': 0,
     'generator-star-spacing': 0,
+    'spaced-comment': 0,
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
 }
