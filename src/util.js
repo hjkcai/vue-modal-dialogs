006085534c7ff55ead1c3d422f12cd7805ea77f4
hjkcai <huajingkun1214@gmail.com>
2017-01-02 00:18:02 +0800

    feat: add 'findIndex' util function

--- src/util.js
+++ src/util.js
@@ -14,3 +14,21 @@ export function find (arr, comparator) {
     }
   }
 }
+
+/**
+ * Find the first item that matches the comparator
+ * and return its index
+ *
+ * @export
+ * @param {Array<any>} arr
+ * @param {Function<boolean>} comparator
+ */
+export function findIndex (arr, comparator) {
+  for (var i = 0; i < arr.length; i++) {
+    if (comparator(arr[i])) {
+      return i
+    }
+  }
+
+  return -1
+}
