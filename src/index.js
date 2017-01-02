3c7fe33760de5b5a314b4926590341b6ab46ca06
hjkcai <huajingkun1214@gmail.com>
2017-01-02 10:36:38 +0800

    refactor: wrap methods from index.js into a class

--- src/index.js
+++ src/index.js
@@ -3,57 +3,62 @@
 import { find } from './util'
 import modalWrapperFactory from './dialogs-wrapper'
 
-let Vue = null
-let modalWrapper = null
-let modalFunctions = []
-
-function findModalByName (name) {
-  return find(modalFunctions, item => item.name === name)
-}
-
-export var debug = process.env.NODE_ENV === 'development'
-
-export function install (vue, options) {
-  // export vue instance to global scope
-  // so that we can easily modify its prototype
-  Vue = vue
-
-  // create an anchor element for modal dialogs' wrapper
-  const anchor = document.createElement('div')
-  document.body.insertBefore(anchor, document.body.childNodes[0])
-
-  // and mount the modal dialogs' wrapper on that anchor
-  const ModalWrapper = modalWrapperFactory(options)
-  modalWrapper = new ModalWrapper()
-  modalWrapper.$mount(anchor)
-}
-
-/**
- * Add a modal function into Vue.prototype
- * so that you can access this function
- * via `this.$<name>` from a Vue component.
- */
-export function use (name, options) {
-  name = name.toString().trim()
-
-  // make sure 'name' is unique
-  if (findModalByName(name)) {
-    if (debug) console.warn(`[vue-modal] Another modal function ${name} is already exist.`)
-    return
+class VueModalDialog {
+  constructor () {
+    this.Vue = null
+    this.debug = process.env.NODE_ENV === 'development'
+    this.modalWrapper = null
+    this.modalFunctions = []
   }
 
-  modalFunctions.push(options)
-  Vue.prototype[`$${name}`] = show.bind(undefined, name)
-}
+  _findModalByName (name) {
+    return find(this.modalFunctions, item => item.name === name)
+  }
 
-export function show (name, ...args) {
-  return new Promise((resolve, reject) => {
-    const modal = findModalByName(name)
-    if (!modal) {
-      if (debug) console.warn(`[vue-modal] Modal dialog ${name} is not found.`)
-      return reject(new Error(`Modal dialog ${name} is not found.`))
+  install (vue, options) {
+    // export vue instance to global scope
+    // so that we can easily modify its prototype
+    this.Vue = vue
+
+    // create an anchor element for modal dialogs' wrapper
+    const anchor = document.createElement('div')
+    document.body.insertBefore(anchor, document.body.childNodes[0])
+
+    // and mount the modal dialogs' wrapper on that anchor
+    const ModalWrapper = modalWrapperFactory(options)
+    this.modalWrapper = new ModalWrapper()
+    this.modalWrapper.$mount(anchor)
+  }
+
+  /**
+   * Add a modal function into Vue.prototype
+   * so that you can access this function
+   * via `this.$<name>` from a Vue component.
+   */
+  use (name, options) {
+    name = name.toString().trim()
+
+    // make sure 'name' is unique
+    if (this._findModalByName(name)) {
+      if (this.debug) console.warn(`[vue-modal] Another modal function ${name} is already exist.`)
+      return
     }
 
-    return modalWrapper.add(modal, ...args)
-  })
+    this.modalFunctions.push(options)
+    this.Vue.prototype[`$${name}`] = this.show.bind(undefined, name)
+  }
+
+  show (name, ...args) {
+    return new Promise((resolve, reject) => {
+      const modal = this._findModalByName(name)
+      if (!modal) {
+        if (this.debug) console.warn(`[vue-modal] Modal dialog ${name} is not found.`)
+        return reject(new Error(`Modal dialog ${name} is not found.`))
+      }
+
+      return this.modalWrapper.add(modal, ...args)
+    })
+  }
 }
+
+export default new VueModalDialog()
