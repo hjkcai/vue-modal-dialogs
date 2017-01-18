b45524c8d478503b5f9ae33eb3f53dac765b0bf4
hjkcai <huajingkun1214@gmail.com>
2017-01-18 13:55:48 +0800

    example: add an element-ui example

---
+++ examples/element-ui/msgbox.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-dialog :title="title" :value="true" @close="close">
+    <p>{{ message }}</p>
+    <span slot="footer">
+      <el-button type="primary" @click="close">OK</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+  export default {
+    props: ['message', 'title'],
+    methods: {
+      close () {
+        this.$emit('close')
+      }
+    }
+  }
+</script>
