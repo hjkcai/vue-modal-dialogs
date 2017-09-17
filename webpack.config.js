ef6688370f1e641c5296d026e81c004773d5166e
hjkcai <huajingkun1214@gmail.com>
2017-09-17 21:57:35 +0800

    fix: wrong example public path

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'docs'),
     filename: '[name].js',
-    publicPath: '/'
+    publicPath: '/vue-modal-dialogs/'
   },
   module: {
     rules: [
