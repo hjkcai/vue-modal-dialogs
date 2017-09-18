62842d482be70080b084cfaa9af46d351c1f485a
hjkcai <huajingkun1214@gmail.com>
2017-09-18 11:00:44 +0800

    feat: update typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,138 +1,139 @@
 declare module 'vue-modal-dialogs' {
   import Vue from 'vue'
 
-  namespace VueModalDialogs {
+  export type TransitionEventHandler = () => void
 
-    export type TransitionEventHandler = () => void
+  /** A Vue component that registered as a dialog */
+  export class DialogComponent<T> extends Vue {
+    /** Close dialog */
+    $close (data: T): void
+  }
 
-    /** A Vue component that registered as a dialog */
-    export class DialogComponent<T> extends Vue {
-      /** Close dialog */
-      $close (data: T): void
-    }
-
-    export interface PluginOptions {
-      /**
-       * Mount point of the wrapper element.
-       * vue-modal-dialogs automatically creates a new element if that element is not present.
-       *
-       * Defaults to `undefined`.
-       */
-      el?: HTMLElement | string,
-
-      /**
-       * Determines if shortcut functions will be added into Vue's prototype.
-       *
-       * Defaults to `true`.
-       */
-      inject?: boolean,
-
-      /**
-       * Render options of the wrapper element.
-       *
-       * This options is the same to VNode's render option.
-       * You can pass any props/events supported by the <transition-group> element.
-       * See https://vuejs.org/v2/guide/render-function.html#The-Data-Object-In-Depth
-       */
-      wrapper?: {
-        class?: Object | string[],
-        style?: Object,
-        attrs?: Object,
-        props?: {
-          tag?: string,
-          name?: string,
-          appear?: boolean,
-          css?: boolean,
-          type?: string,
-          moveClass?: string,
-          enterClass?: string,
-          leaveClass?: string,
-          enterToClass?: string,
-          leaveToClass?: string,
-          enterActiveClass?: string,
-          leaveActiveClass?: string,
-          appearClass?: string,
-          appearActiveClass?: string,
-          appearToClass?: string
-        },
-        domProps?: Object,
-        on?: {
-          beforeEnter?: TransitionEventHandler,
-          enter?: TransitionEventHandler,
-          afterEnter?: TransitionEventHandler,
-          beforeLeave?: TransitionEventHandler,
-          leave?: TransitionEventHandler,
-          afterLeave?: TransitionEventHandler,
-          beforeAppear?: TransitionEventHandler,
-          appear?: TransitionEventHandler,
-          afterAppear?: TransitionEventHandler
-        },
-        nativeOn?: Object,
-        directives?: Vue.VNodeDirective[],
-        ref?: string
-      }
-
-      /** Options to control the `z-index` css property of each dialog */
-      zIndex?: {
-        /**
-         * The initial value of `z-index`.
-         *
-         * Defaults to `1000`.
-         */
-        value?: number,
-
-        /**
-         * Indicates if the `z-index` auto increses
-         * when a new modal dialog is shown.
-         *
-         * Defaults to `true`.
-         */
-        autoIncrement?: boolean
-      }
-    }
+  export interface PluginOptions {
+    /**
+     * Mount point of the wrapper element.
+     * vue-modal-dialogs automatically creates a new element if that element is not present.
+     *
+     * Defaults to `undefined`.
+     */
+    el?: HTMLElement | string,
 
     /**
-     * Options to build a dialog function
+     * Determines if shortcut functions will be added into Vue's prototype.
      *
-     * This is almost all the same to the VNode's render options.
+     * Defaults to `true`.
+     */
+    inject?: boolean,
+
+    /**
+     * Render options of the wrapper element.
+     *
+     * This options is the same to VNode's render option.
+     * You can pass any props/events supported by the <transition-group> element.
      * See https://vuejs.org/v2/guide/render-function.html#The-Data-Object-In-Depth
      */
-    export interface DialogRenderOptions<T> {
-      /** A Vue component that will be the 'dialog component' of a modal dialog */
-      component: DialogComponent<T>,
-
-      /** An array that maps the argument list to props */
-      args: string[],
-
-      /**
-       * Indicates whether to inject dialog methods into Vue's prototype.
-       * This can override the global setting
-       */
-      inject?: boolean,
-
+    wrapper?: {
       class?: Object | string[],
       style?: Object,
       attrs?: Object,
-      props?: Object,
+      props?: {
+        tag?: string,
+        name?: string,
+        appear?: boolean,
+        css?: boolean,
+        type?: string,
+        moveClass?: string,
+        enterClass?: string,
+        leaveClass?: string,
+        enterToClass?: string,
+        leaveToClass?: string,
+        enterActiveClass?: string,
+        leaveActiveClass?: string,
+        appearClass?: string,
+        appearActiveClass?: string,
+        appearToClass?: string
+      },
       domProps?: Object,
-      on?: Object,
+      on?: {
+        beforeEnter?: TransitionEventHandler,
+        enter?: TransitionEventHandler,
+        afterEnter?: TransitionEventHandler,
+        beforeLeave?: TransitionEventHandler,
+        leave?: TransitionEventHandler,
+        afterLeave?: TransitionEventHandler,
+        beforeAppear?: TransitionEventHandler,
+        appear?: TransitionEventHandler,
+        afterAppear?: TransitionEventHandler
+      },
       nativeOn?: Object,
       directives?: Vue.VNodeDirective[],
       ref?: string
     }
 
-    export interface DialogFunction<T> {
-      (...args: any[]): Promise<T>
+    /** Options to control the `z-index` css property of each dialog */
+    zIndex?: {
+      /**
+       * The initial value of `z-index`.
+       *
+       * Defaults to `1000`.
+       */
+      value?: number,
+
+      /**
+       * Indicates if the `z-index` auto increses
+       * when a new modal dialog is shown.
+       *
+       * Defaults to `true`.
+       */
+      autoIncrement?: boolean
     }
+  }
+
+  /**
+   * Options to build a dialog function
+   *
+   * This is almost all the same to the VNode's render options.
+   * See https://vuejs.org/v2/guide/render-function.html#The-Data-Object-In-Depth
+   */
+  export interface DialogRenderOptions<T> {
+    /** A Vue component that will be the 'dialog component' of a modal dialog */
+    component: DialogComponent<T>,
+
+    /** An array that maps the argument list to props */
+    args: string[],
+
+    /**
+     * Indicates whether to inject dialog methods into Vue's prototype.
+     * This can override the global setting
+     */
+    inject?: boolean,
+
+    class?: Object | string[],
+    style?: Object,
+    attrs?: Object,
+    props?: Object,
+    domProps?: Object,
+    on?: Object,
+    nativeOn?: Object,
+    directives?: Vue.VNodeDirective[],
+    ref?: string
+  }
+
+  export interface DialogFunction<T> {
+    (...args: any[]): Promise<T>
+  }
+
+  interface ModalDialogs {
+    [key: string]: DialogFunction<any> | any,
 
     /**
      * Indicates if debug mode is turned on.
      * This results in some console warns to be shown
      */
-    export var debug: boolean
+    debug: boolean,
 
     /** Install `vue-modal-dialogs` into Vue */
-    export function install (vue: Vue, options: PluginOptions): void
+    install (vue: Vue, options: PluginOptions): void,
 
     /**
      * Add a dialog function
@@ -140,10 +141,10 @@ declare module 'vue-modal-dialogs' {
      * @param component A Vue component that will be the 'template' of a modal dialog
      * @param args An array that maps the argument list to props
      */
-    export function add<T> (name: string, component: DialogComponent<T>, ...args: string[]): DialogFunction<T> | undefined
+    add<T> (name: string, component: DialogComponent<T>, ...args: string[]): DialogFunction<T> | undefined,
 
     /** Add a dialog function */
-    export function add<T> (name: string, options: DialogRenderOptions<T>): DialogFunction<T> | undefined
+    add<T> (name: string, options: DialogRenderOptions<T>): DialogFunction<T> | undefined,
 
     /**
      * Show a modal dialog
@@ -152,8 +153,9 @@ declare module 'vue-modal-dialogs' {
      * @param args The arguments to be passed
      * @returns Returns a promise that resolves when the modal dialog is closed
      */
-    export function show<T> (name: string, ...args: any[]): Promise<T>
+    show<T> (name: string, ...args: any[]): Promise<T>
   }
 
-  export = VueModalDialogs
+  const modalDialogs: ModalDialogs
+  export default modalDialogs
 }
