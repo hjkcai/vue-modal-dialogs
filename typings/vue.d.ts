4c6fdec0dd7371dd7b4f433a13bf2f6864a94ed5
hjkcai <huajingkun1214@gmail.com>
2017-09-25 12:03:41 +0800

    feat: update types definition to match the new api

--- typings/vue.d.ts
+++
@@ -1,8 +0,0 @@
-import Vue = require('vue')
-import { ModalDialogsInstance } from 'vue-modal-dialogs'
-
-declare module "vue/types/vue" {
-  interface Vue {
-    $dialogs: ModalDialogsInstance
-  }
-}
---
+++ vue-modal-dialogs.d.ts
@@ -0,0 +1,119 @@
+import Vue from 'vue'
+export as namespace VueModalDialogs
+
+export type TransitionEventHandler = () => void
+
+interface WrapperRenderOptions extends Vue.VNodeData {
+  props?: {
+    tag?: string,
+    name?: string,
+    appear?: boolean,
+    css?: boolean,
+    type?: string,
+    moveClass?: string,
+    enterClass?: string,
+    leaveClass?: string,
+    enterToClass?: string,
+    leaveToClass?: string,
+    enterActiveClass?: string,
+    leaveActiveClass?: string,
+    appearClass?: string,
+    appearActiveClass?: string,
+    appearToClass?: string
+  },
+  on?: {
+    beforeEnter?: TransitionEventHandler,
+    enter?: TransitionEventHandler,
+    afterEnter?: TransitionEventHandler,
+    beforeLeave?: TransitionEventHandler,
+    leave?: TransitionEventHandler,
+    afterLeave?: TransitionEventHandler,
+    beforeAppear?: TransitionEventHandler,
+    appear?: TransitionEventHandler,
+    afterAppear?: TransitionEventHandler
+  }
+}
+
+export interface PluginOptions {
+  /**
+   * Mount point of the wrapper element.
+   * vue-modal-dialogs automatically creates a new element if that element is not present.
+   *
+   * Defaults to `undefined`.
+   */
+  el?: HTMLElement | string,
+
+  /**
+   * Render options of the wrapper element.
+   *
+   * This options is the same to VNode's render option.
+   * You can pass any props/events supported by the <transition-group> element.
+   * See https://vuejs.org/v2/guide/render-function.html#The-Data-Object-In-Depth
+   */
+  wrapper?: WrapperRenderOptions
+
+  /** Options to control the `z-index` css property of each dialog */
+  zIndex?: {
+    /**
+     * The initial value of `z-index`.
+     *
+     * Defaults to `1000`.
+     */
+    value?: number,
+
+    /**
+     * Indicates if the `z-index` auto increses
+     * when a new modal dialog is shown.
+     *
+     * Defaults to `true`.
+     */
+    autoIncrement?: boolean
+  }
+}
+
+/** A Vue component that registered as a dialog */
+export class DialogComponent<T> extends Vue {
+  /** Close dialog */
+  $close (data: T): Promise<T>
+}
+
+/** Options to build a dialog function */
+export interface DialogOptions<T> {
+  /** A Vue component that will be the 'dialog component' of a modal dialog */
+  component: DialogComponent<T>,
+
+  /** An array that maps the argument list to props */
+  props: string[],
+
+  /**
+   * Options to render the dialog component.
+   *
+   * This is the same to the VNode's render options.
+   * See https://vuejs.org/v2/guide/render-function.html#The-Data-Object-In-Depth
+   */
+  render: Vue.VNodeData
+}
+
+export interface DialogFunction<T> {
+  (...args: any[]): Promise<T>
+}
+
+export interface ModalDialogsInstance {
+  /** Install `vue-modal-dialogs` into Vue */
+  install (vue: typeof Vue, options?: PluginOptions): void,
+
+  /** Create a dialog function */
+  makeDialog<T> (component: Vue.Component, ...props: string[]): DialogFunction<T>,
+
+  /** Create a dialog function */
+  makeDialog<T> (options: DialogOptions<T>): DialogFunction<T>
+}
+
+declare const modalDialogs: ModalDialogsInstance
+export default modalDialogs
+
+/** Create a dialog function */
+export function makeDialog<T> (component: Vue.Component, ...props: string[]): DialogFunction<T>
+
+/** Create a dialog function */
+export function makeDialog<T> (options: DialogOptions<T>): DialogFunction<T>
