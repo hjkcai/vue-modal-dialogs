6f520b77fb0071abc0cb550b03720b19ca552f8c
hjkcai <huajingkun1214@gmail.com>
2017-10-05 22:05:49 +0800

    fix: errors in typings

--- vue-modal-dialogs.d.ts
+++ vue-modal-dialogs.d.ts
@@ -36,7 +36,7 @@ interface WrapperRenderOptions extends Vue.VNodeData {
 
 export interface PluginOptions {
   /**
-   * Mount point of the wrapper element.
+   * Mount point of the wrapper element. All dialogs will be inside this wrapper.
    * vue-modal-dialogs automatically creates a new element if that element is not present.
    *
    * Defaults to `undefined`.
@@ -47,12 +47,16 @@ export interface PluginOptions {
    * Render options of the wrapper element.
    *
    * This options is the same to VNode's render option.
-   * You can pass any props/events supported by the <transition-group> element.
+   * You can pass any props/events supported by the <transition-group> component.
    * See https://vuejs.org/v2/guide/render-function.html#The-Data-Object-In-Depth
    */
   wrapper?: WrapperRenderOptions
 
-  /** Options to control the `z-index` css property of each dialog */
+  /**
+   * Options to control the `z-index` css property of each dialog.
+   * This feature guarantees that the newer dialog is always on the top of the older dialogs.
+   * You can disable this feature by setting this option to `false`.
+   */
   zIndex?: {
     /**
      * The initial value of `z-index`.
@@ -68,7 +72,7 @@ export interface PluginOptions {
      * Defaults to `true`.
      */
     autoIncrement?: boolean
-  }
+  } | false
 }
 
 /** A Vue component that registered as a dialog */
@@ -101,7 +105,7 @@ export interface DialogOptions<T> {
    * This is the same to the VNode's render options.
    * See https://vuejs.org/v2/guide/render-function.html#The-Data-Object-In-Depth
    */
-  render: Vue.VNodeData
+  render?: Vue.VNodeData
 }
 
 export interface DialogFunction<T> {
