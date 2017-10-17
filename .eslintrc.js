58f60298f1a43e55e9b776e08f9b50918bf2aa83
hjkcai <huajingkun1214@gmail.com>
2017-10-17 21:46:57 +0800

    chore: update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     'arrow-parens': 0,
     'generator-star-spacing': 0,
     'spaced-comment': 0,
+    'promise/param-names': 0,
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
 }
