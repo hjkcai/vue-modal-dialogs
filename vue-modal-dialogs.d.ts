7c6a8521f83b9f1b62d80f3e644f04327e2bcc5a
hjkcai <huajingkun1214@gmail.com>
2017-11-15 08:38:04 +0800

    feat: update types definition to match the new api

--- vue-modal-dialogs.d.ts
+++ vue-modal-dialogs.d.ts
@@ -1,54 +1,12 @@
-import Vue from 'vue'
+import Vue, { ComponentOptions } from 'vue'
+import { VueConstructor, Vue as _Vue, ExtendedVue } from 'vue/types/vue'
 export as namespace VueModalDialogs
 
-export interface PluginOptions {
-  /**
-   * Mount point of the wrapper element. All dialogs will be inside this wrapper.
-   * vue-modal-dialogs automatically creates a new element if that element is not present.
-   *
-   * Defaults to `undefined`.
-   */
-  el?: HTMLElement | string,
-
-  /**
-   * Render options of the wrapper element.
-   *
-   * This options is the same to VNode's render option.
-   * You can pass any props/events supported by the <transition-group> component.
-   * See https://vuejs.org/v2/guide/render-function.html#The-Data-Object-In-Depth
-   */
-  wrapper?: Vue.VNodeData
-
-  /**
-   * Component options of the dialog wrapper component.
-   */
-  wrapperComponentOptions?: Vue.ComponentOptions<Vue>
-
-  /**
-   * Options to control the `z-index` css property of each dialog.
-   * This feature guarantees that the newer dialog is always on the top of the older dialogs.
-   * You can disable this feature by setting this option to `false`.
-   */
-  zIndex?: {
-    /**
-     * The initial value of `z-index`.
-     *
-     * Defaults to `1000`.
-     */
-    value?: number,
-
-    /**
-     * Indicates if the `z-index` auto increases
-     * when a new modal dialog is shown.
-     *
-     * Defaults to `true`.
-     */
-    autoIncrement?: boolean
-  } | false
+export interface DialogPromise<ReturnType> extends Promise<ReturnType> {
+  close (data: ReturnType): DialogPromise<ReturnType>
 }
 
-/** A Vue component that registered as a dialog */
-export class DialogComponent<T> extends Vue {
+export interface DialogComponent<ReturnType> extends Vue {
   /** The unique id of this dialog */
   readonly dialogId: number
 
@@ -56,50 +14,90 @@ export class DialogComponent<T> extends Vue {
   readonly arguments: any[]
 
   /** Close dialog */
-  $close (data: T): void
+  $close (data: ReturnType): void
 }
 
-export interface DialogPromise<T> extends Promise<T> {
-  close (data: T): DialogPromise<T>
-}
+export type Component<ReturnType, PropsDef> = ComponentOptions<DialogComponent<ReturnType> & PropsDef> | VueConstructor
 
 /** Options to build a dialog function */
-export interface DialogOptions<T> {
+export interface DialogOptions<ReturnType, PropsDef> {
   /** A Vue component that will be the 'dialog component' */
-  component: DialogComponent<T>,
+  component: Component<ReturnType, PropsDef>,
 
   /** An array that maps the argument list to props */
   props: string[],
 
-  /**
-   * Options to render the dialog component.
-   *
-   * This is the same to the VNode's render options.
-   * See https://vuejs.org/v2/guide/render-function.html#The-Data-Object-In-Depth
-   */
-  render?: Vue.VNodeData
+  /** The wrapper that the dialog will be added into */
+  wrapper: string[]
 }
 
-export interface DialogFunction<T> {
-  (...args: any[]): DialogPromise<T>
+interface DialogFunction<ReturnType = any, PropsDef extends object = {}> {
+  (...args: any[]): DialogPromise<ReturnType>
+  (data?: PropsDef): DialogPromise<ReturnType>
 }
+
+export declare function makeDialog<
+  ReturnType = any,
+  PropsDef extends object = {}
+> (
+  options: DialogOptions<ReturnType, PropsDef>
+): DialogFunction<ReturnType, PropsDef>
+
+export declare function makeDialog<
+  ReturnType = any,
+  PropsDef extends object = {}
+> (
+  component: Component<ReturnType, PropsDef>
+): (data?: PropsDef) => DialogPromise<ReturnType>
+
+export declare function makeDialog<
+  ReturnType = any,
+  Arg1 = any,
+  PropsDef extends object = {}
+> (
+  component: Component<ReturnType, PropsDef>,
+  prop1: string
+): (arg1: Arg1) => DialogPromise<ReturnType>
+
+export declare function makeDialog<
+  ReturnType = any,
+  Arg1 = any,
+  Arg2 = any,
+  PropsDef extends object = {}
+> (
+  component: Component<ReturnType, PropsDef>,
+  prop1: string,
+  prop2: string
+): (arg1: Arg1, arg2: Arg2) => DialogPromise<ReturnType>
+
+export declare function makeDialog<
+  ReturnType = any,
+  Arg1 = any,
+  Arg2 = any,
+  Arg3 = any,
+  PropsDef extends object = {}
+> (
+  component: Component<ReturnType, PropsDef>,
+  prop1: string,
+  prop2: string,
+  prop3: string
+): (arg1: Arg1, arg2: Arg2, arg3: Arg3) => DialogPromise<ReturnType>
+
+export declare function makeDialog<
+  ReturnType = any,
+  PropsDef extends object = {}
+> (
+  component: Component<ReturnType, PropsDef>,
+  ...props: string[]
+): DialogFunction<ReturnType, PropsDef>
+
+/** Dialogs wrapper component */
+export declare const DialogsWrapper: ExtendedVue<Vue, {}, {}, {}, {}>
 
 export interface ModalDialogsInstance {
   /** Install `vue-modal-dialogs` into Vue */
-  install (vue: typeof Vue, options?: PluginOptions): void,
-
-  /** Create a dialog function */
-  makeDialog<T> (component: Vue.Component, ...props: string[]): DialogFunction<T>,
-
-  /** Create a dialog function */
-  makeDialog<T> (options: DialogOptions<T>): DialogFunction<T>
+  install (vue: typeof _Vue): void
 }
 
 declare const modalDialogs: ModalDialogsInstance
 export default modalDialogs
-
-/** Create a dialog function */
-export function makeDialog<T> (component: Vue.Component, ...props: string[]): DialogFunction<T>
-
-/** Create a dialog function */
-export function makeDialog<T> (options: DialogOptions<T>): DialogFunction<T>
