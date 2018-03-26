f945a25681398f0cd3d43a51ba6e804562676634
hjkcai <huajingkun1214@gmail.com>
2018-03-27 00:05:32 +0800

    feat: use a more realiable algorithm to filter undefined props deps: remove arr-diff

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,5 @@
 'use strict'
 
-import diff from 'arr-diff'
-
 export function noop () { /* nothing */ }
 
 export const CLOSE_EVENT = 'vue-modal-dialogs:close'
@@ -52,6 +50,44 @@ export function isVueConstructor (obj) {
   }
 }
 
+export function filterUndefinedProps (props, component) {
+  const propKeys = {}
+  const queue = [component]
+
+  function pushIfNotNull (value) {
+    if (value != null) {
+      queue.push(value)
+    }
+  }
+
+  // eslint-disable-next-line no-cond-assign
+  while (component = queue.shift()) {
+    let propKeysArray
+    if (Array.isArray(component.props)) {
+      propKeysArray = component.props
+    } else if (typeof component.props === 'object') {
+      propKeysArray = Object.keys(component.props)
+    }
+
+    if (propKeysArray) {
+      propKeysArray.forEach(key => {
+        propKeys[key] = true
+      })
+    }
+
+    // If component is from Vue.extend, the real options are in component.options
+    pushIfNotNull(component.options)
+
+    // Iterate over component's parent compoent and mixins
+    pushIfNotNull(component.extends)
+    if (Array.isArray(component.mixins)) {
+      component.mixins.forEach(pushIfNotNull)
+    }
+  }
+
+  return props.filter(key => !propKeys[key])
+}
+
 export function generateDialogData (props, component) {
   let dialogData
 
@@ -61,7 +97,7 @@ export function generateDialogData (props, component) {
     createdCallback: noop,
     component: Promise.resolve(component).then(component => ({
       extends: component.default || component,
-      props: diff(['dialogId', 'arguments', ...props], Object.keys(component.props || (component.options && component.options.props) || [])),
+      props: filterUndefinedProps(['dialogId', 'arguments', ...props], component),
       created () {
         // Resolves componentPromise that is created in DialogsWrapper.add()
         dialogData.createdCallback(this)
