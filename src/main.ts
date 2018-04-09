db9edc054b427da5e2a70ae430e8861ad6a8c6ae
Jingkun Hua <huajingkun1214@gmail.com>
2018-04-09 16:57:29 +0800

    feat: change the entry to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+// The entry must be a .ts file. Otherwise TypeScript in .vue file will not work
+// See https://github.com/TypeStrong/ts-loader/issues/505 for more detail
+
 import 'element-ui/lib/theme-chalk/index.css'
 import 'highlight.js/styles/color-brewer.css'
 
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import ElementUI from 'element-ui'
 import * as ModalDialogs from 'vue-modal-dialogs'
 
